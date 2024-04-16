3184e4f97e3aeed7c2c97c20083ec1f3d3c80619
Giovana Padilha da Silva <167197637+gipadilha0@users.noreply.github.com>
2024-04-16 09:04:25 -0300

    giova

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual o melhor time?");
+    var respostaTime = prompt("Quais os tres estados mais populosos do Brasil?");
 
     
-    if (respostaTime.toLowerCase() === "athlético pr") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
+    if (respostaTime.toLowerCase() === "São Paulo,Rio de Janeiro e Minas Gerais") {
+      alert("Isso mesmo! Esses são os 3 estados mais populosos do Brasil!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
